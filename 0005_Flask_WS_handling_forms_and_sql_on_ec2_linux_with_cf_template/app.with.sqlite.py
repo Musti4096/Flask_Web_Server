c752f235e0128ccb50f49cafec461f254290772e
Mustafa <mustafaak4096@gmail.com>
2020-09-24 17:11:06 +0300

    files

--- 0005_Flask_WS_handling_forms_and_sql_on_ec2_linux_with_cf_template/app.with.sqlite.py
+++ 0005_Flask_WS_handling_forms_and_sql_on_ec2_linux_with_cf_template/app.with.sqlite.py
@@ -81,5 +81,4 @@ def add_emails():
         return render_template("add-email.html", show_result = False)
 
 if __name__ == "__main__":
-    #app.run(debug=True)
     app.run(host=0.0.0.0/0, port=80)
